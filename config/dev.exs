94ddc5efde0f6ef6d045897e05d260b7cbbae386
Kevin Koltz <kevin.koltz@gmail.com>
2019-07-29 22:14:06 -0500

    Remove extra directory in reload watcher

--- config/dev.exs
+++ config/dev.exs
@@ -61,8 +61,7 @@ config :live_tracker, LiveTrackerWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/live_tracker_web/{live,views}/.*(ex)$",
-      ~r"lib/live_tracker_web/templates/.*(eex)$",
-      ~r{lib/live_view/live/.*(ex)$}
+      ~r"lib/live_tracker_web/templates/.*(eex)$"
     ]
   ]
 
