2e0446dd6856b386215703583e71fbf894173347
Kevin Koltz <kevin.koltz@gmail.com>
2019-10-05 12:14:00 -0500

    Add support for previous Gigalixir config

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,18 @@
 use Mix.Config
 
-gigalixir_host = System.get_env("APP_NAME") && System.get_env("APP_NAME") <> ".gigalixirapp.com"
-render_host = System.get_env("RENDER_EXTERNAL_HOSTNAME") && "livetracker.kevinkoltz.com"
-host = render_host || gigalixir_host || "localhost"
-
-config :live_tracker, LiveTrackerWeb.Endpoint,
-  url: [host: host, port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+if System.get_env("RENDER_EXTERNAL_HOSTNAME") do
+  # Render config
+  config :live_tracker, LiveTrackerWeb.Endpoint,
+    url: [host: "livetracker.kevinkoltz.com", port: 80],
+    cache_static_manifest: "priv/static/cache_manifest.json"
+else
+  # Gigalixir config (old config)
+  config :live_tracker, LiveTrackerWeb.Endpoint,
+    http: [port: {:system, "PORT"}],
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+    secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+    cache_static_manifest: "priv/static/cache_manifest.json",
+    server: true
+end
 
 config :logger, level: :debug
